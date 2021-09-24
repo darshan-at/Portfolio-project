2a87acd116044f954baa54c1929faae1e659697f
darshan-at <43428382+darshan-at@users.noreply.github.com>
2021-09-24 18:56:44 +0530

    Home ,Education ,Skill added
    Some initial built of Home ,Education and Skill section. It still need changes.

---
+++ scripts/myscript.js
@@ -0,0 +1,43 @@
+const navbar=document.querySelector(".my-nav");
+const tabs=document.querySelectorAll(".tab");
+const contents=document.querySelectorAll(".container");
+navbar.onclick=function(ev){
+    const id=ev.target.id;
+    if(id)
+    {
+        /*change tab active color*/
+        tabs.forEach(btn=>{
+            btn.classList.remove("active");
+        });
+        ev.target.classList.add("active");
+
+        /*change content display to block*/
+        contents.forEach(ctn => {
+            ctn.classList.remove("active");
+        });
+        let ele=document.getElementById(id+"-cont");
+        ele.classList.add("active");
+    }
+};
+
+function changeEdu(card)
+{
+    console.log(card.id);
+    let eduPhoto=document.getElementById("edu-photo");
+    eduPhoto.setAttribute("src","./images/"+card.id+".jpg");
+    let cards=document.querySelectorAll(".card");
+
+    cards.forEach(element => {
+        element.classList.remove("edu-active");
+    });
+    card.classList.add("edu-active");
+
+}
+function resNav() {
+    var x = document.querySelector(".my-nav");
+    if (x.className === "my-nav") {
+        x.classList.add("nav-res");
+    } else {
+      x.className = "my-nav";
+    }
+  }
